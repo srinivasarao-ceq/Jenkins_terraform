d30288ecb0d87ea77c41c51ad41b5e09994bcad5
srinivasarao-ceq <123088975+srinivasarao-ceq@users.noreply.github.com>
2023-07-20 15:57:08 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,7 +5,11 @@ terraform {
       version = "5.8.0"
     }
   }
-
+   backend "s3" {
+    bucket = "msr-tu-jenkins"
+    key    = "jenkins/terraform.tfstate"
+    region = "eu-central-1"
+  }
 }
 
 provider "aws" {
